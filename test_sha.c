1ac12279f9d07537dd556ca0f62473e27965cf2b
graes1234 <117164702+graes1234@users.noreply.github.com>
2025-12-04 17:54:25 +0900

    Update test_sha.c

--- test_sha.c
+++ test_sha.c
@@ -1,30 +1,4 @@
-// Compile: gcc sha256_test.c -o sha256_test -lcrypto
 //SHA-256 무결성 체크 테스트
-/*#include <stdio.h>
-#include <string.h>
-#include <openssl/evp.h>
-
-void print_hex(const unsigned char *buf, int len) {
-    for (int i=0;i<len;i++)
-        printf("%02x", buf[i]);
-    printf("\n");
-}
-
-int main() {
-    const char *msg = "this is integrity test";
-    unsigned char hash[32];
-
-    EVP_MD_CTX *md = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(md, EVP_sha256(), NULL);
-    EVP_DigestUpdate(md, msg, strlen(msg));
-    unsigned int outlen = 0;
-    EVP_DigestFinal_ex(md, hash, &outlen);
-    EVP_MD_CTX_free(md);
-    printf("Message: %s\nSHA256: ", msg);
-    print_hex(hash, 32);
-    return 0;
-}*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
